4e0a629af00e5175cbf89267aa590bcadd572199
aimad.boulaadam02@gmail.com <aimad.boulaadam02@gmail.com>
2026-03-27 16:59:28 +0100

    Add exam detail and delete exam URL routes

--- ExamSolver/exam_app/urls.py
+++ ExamSolver/exam_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.upload_view, name='upload'),
+    path('exam/<int:pk>/', views.exam_detail_view, name='exam_detail'),
+    path('exam/<int:pk>/delete/', views.delete_exam_view, name='delete_exam'),
+   
    ]
