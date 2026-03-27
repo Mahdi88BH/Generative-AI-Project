3b5c250f66a2b396bff4815f26a82133a1163ef5
aimad.boulaadam02@gmail.com <aimad.boulaadam02@gmail.com>
2026-03-27 17:02:25 +0100

    Add login and logout URL routes to exam application

--- ExamSolver/exam_app/urls.py
+++ ExamSolver/exam_app/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('exam/<int:pk>/', views.exam_detail_view, name='exam_detail'),
     path('exam/<int:pk>/delete/', views.delete_exam_view, name='delete_exam'),
     path('register/', views.register_view, name='register'),
-   
+    path('login/', views.login_view, name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+
    ]
