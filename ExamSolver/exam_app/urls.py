aa5b4298f67448c0f8f7746175028e4a8e894104
aimad.boulaadam02@gmail.com <aimad.boulaadam02@gmail.com>
2026-03-27 17:00:01 +0100

    Add registration URL route to exam application

--- ExamSolver/exam_app/urls.py
+++ ExamSolver/exam_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.upload_view, name='upload'),
     path('exam/<int:pk>/', views.exam_detail_view, name='exam_detail'),
     path('exam/<int:pk>/delete/', views.delete_exam_view, name='delete_exam'),
+    path('register/', views.register_view, name='register'),
    
    ]
