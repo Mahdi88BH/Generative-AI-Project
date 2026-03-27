b0431ec1de2494a1384c0f561e0cdc2aedeb483b
aimad.boulaadam02@gmail.com <aimad.boulaadam02@gmail.com>
2026-03-27 16:47:46 +0100

    Refactor URL configuration and enhance Exam model with necessary fields

--- ExamSolver/exam_app/models.py
+++ ExamSolver/exam_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Exam(models.Model):
+    # Correction ici : on_delete au lieu de on_get
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="exams")
+    image = models.ImageField(upload_to='exams/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    result_text = models.TextField(null=True, blank=True)
+    status = models.CharField(max_length=20, default='Pending') 
+
+    def __str__(self):
+        # Utilisation de self.user.username pour un affichage clair dans l'admin
+        return f"Exam {self.id} by {self.user.username}"
