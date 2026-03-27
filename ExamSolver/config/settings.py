6647fd9bddeb112f060ff2ea7a9df56651292495
aimad.boulaadam02@gmail.com <aimad.boulaadam02@gmail.com>
2026-03-27 17:23:44 +0100

    Add template directory to Django settings and create initial migration for Exam model

--- ExamSolver/config/settings.py
+++ ExamSolver/config/settings.py
@@ -55,10 +55,11 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # <--- AJOUTE CECI
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug', # Optionnel mais recommandé
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
